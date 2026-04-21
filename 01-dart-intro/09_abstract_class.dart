983f7a5503bd94d1a3e1220ee8aff978bd8c1a2f
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-04-21 11:46:00 -0400

    Update 09_abstract_class.dart

--- 01-dart-intro/09_abstract_class.dart
+++ 01-dart-intro/09_abstract_class.dart
@@ -1,21 +1,21 @@
 void main(){
     final windplant = WindPlant(initialEnergy: 100);
+    final nuclearplant = NuclearPlant(energyLeft: 1000);
 
     print('wind: ${chargePhone(windplant)}');
+    print('nuclear: ${chargePhone(nuclearplant)}');
 }
-
 
 double chargePhone(EnergyPlant plant){
     if ( plant.energyLeft < 10 ) throw Exception('Not enough energy');
     return plant.energyLeft - 10;
 }
 
-
 enum PlantType { nuclear, wind, water }
 
 abstract class EnergyPlant{
     double energyLeft;
-    PlantType type; // nuclear, wind, water
+    final PlantType type; // nuclear, wind, water
 
     EnergyPlant({
         required this.energyLeft, 
@@ -28,7 +28,6 @@ abstract class EnergyPlant{
 
 // extends o implements
 class WindPlant extends EnergyPlant {
-    
     WindPlant({required double initialEnergy})
         : super( energyLeft: initialEnergy, type: PlantType.wind );
 
@@ -38,3 +37,18 @@ class WindPlant extends EnergyPlant {
     }
 }
 
+class NuclearPlant implements EnergyPlant {
+  @override
+  double energyLeft;
+  @override
+  final PlantType type = PlantType.nuclear;
+
+  NuclearPlant({required this.energyLeft});
+
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft -= amount;
+  }
+
+
+}
