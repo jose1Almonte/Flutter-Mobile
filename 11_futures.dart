6789c5e2681465725cb0d1fc9d1dc7a9109fbf56
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-04-23 12:36:19 -0400

    future

--- 11_futures.dart
+++ 11_futures.dart
@@ -11,7 +11,7 @@ void main(){
 Future<String> httpGet( String url ){
   return Future.delayed( const Duration(seconds: 1), () {
     throw 'error en la peticion http';
-    // return 'respuesta de la peticion http';
+    // return 'respuesta de la peticiosn http';
   });
 }
 
