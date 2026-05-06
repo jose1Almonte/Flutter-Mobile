dfeac1912678d7fb06030dc86998dd9821735bf1
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-05-06 14:28:34 -0400

    Contador, StatefulWidget

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
 
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const[
-            Text('10', style: TextStyle( fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Clicks', style: TextStyle( fontSize: 25)),
+          children: [
+            Text('$clickCounter', style: const TextStyle( fontSize: 160, fontWeight: FontWeight.w100)),
+            Text((clickCounter == 1) ? 'Click' : 'Clicks', style: const TextStyle( fontSize: 25)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          
+          setState(() {
+            clickCounter++;
+          });
         },
         child: const Icon(Icons.plus_one), 
       ),
