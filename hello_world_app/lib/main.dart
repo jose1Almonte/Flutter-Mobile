8e1edde7d193ba76f0ed2b420cbb22eb4fe5a45f
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-05-06 16:08:21 -0400

    Nuevos botones para el contador
    Se utiliza FloatingActionButtons repetidas veces

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue
       ),
-      home: const CounterScreen()
+      home: const CounterFunctionsScreen()
     );
   }
 }
