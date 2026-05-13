8bc4a9f65300cdea0ccaa4cde78fa8053fc62503
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-05-13 16:20:01 -0400

    yes_no_app loadingBuilder
    loadingBuilder y tambien se agregaron los mensajes de ella

---
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: colors.secondary,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: Text('Labore sint eiusmod ipsum magna sit labore Lorem ipsum.',style: TextStyle(color: Colors.white),),
+          ),
+        ),
+        SizedBox(height: 5,),
+
+        _ImageBubble(),
+
+        SizedBox(height: 10,)
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network('https://yesno.wtf/assets/no/4-122be48db47678331dbef3dd6ac6ff56.gif',
+              width: size.width*0.7,
+              height: 150,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) return child;
+
+                return Container(
+                  width: size.width*0.7,
+                  height: 150,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                  child: Text('Mi amor está enviando una imagen'),
+                );
+                
+
+              },
+      ));  }
+}
