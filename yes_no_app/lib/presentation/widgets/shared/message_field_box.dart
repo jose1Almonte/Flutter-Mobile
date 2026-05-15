df2060b2cf30f245f125abcfffa1df36ae26c2d7
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-05-15 12:50:45 -0400

    Update message_field_box.dart
    Se añadieron funcionalidades para el comportamiento a la hora de escribir en el TextFormField

--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -6,30 +6,42 @@ class MessageFieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-  final colors = Theme.of(context).colorScheme;
-  final outlinedInputBorder = UnderlineInputBorder(
-    borderSide: BorderSide( color: Colors.transparent ),
-    borderRadius: BorderRadius.circular(20)
-  );
+    final textController = TextEditingController(); 
+    final focusNode = FocusNode();
 
-  final inputDecoration = InputDecoration(
-        // enabledBorder: outlinedInputBorder,
-        focusedBorder: outlinedInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send_outlined),
-          onPressed: (){},
-        )
-      );
+    final colors = Theme.of(context).colorScheme;
+    final outlinedInputBorder = UnderlineInputBorder(
+      borderSide: BorderSide( color: Colors.transparent ),
+      borderRadius: BorderRadius.circular(20)
+    );
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
+      // enabledBorder: outlinedInputBorder,
+      focusedBorder: outlinedInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: (){
+          final textValue = textController.value.text;
+          print(textValue);
+          textController.clear();
+        },
+      )
+    );
 
     return TextFormField(
+      onTapOutside: (event){
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        print('Submit value: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
-      onChanged: (value) {
-        print('changed value: $value');
-      },
+      
     );
   }
 }
