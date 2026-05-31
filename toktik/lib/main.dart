2e48f70b2953903c2c524a6c5cb43c8c0e6db189
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-05-31 19:36:58 -0400

    first widget_app commit

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+import 'package:toktik/infrastructure/datasources/local_videos_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/provider/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,9 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDatasource: LocalVideosDatasourceImpl()
+    );
+
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => DiscoverProvider()..loadNextPage(),lazy: false,)
+        ChangeNotifierProvider(create: (_) => DiscoverProvider(videosRepository: videoPostRepository)..loadNextPage(),lazy: false,)
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
--- toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -40,19 +41,29 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2,),);
         }
 
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(controller),
-              // Gradiente
-              // Texto
-              Positioned(
-                bottom: 50,
-                left: 20,
-                child: _VideoCaption(caption: widget.caption)
-                )
-            ],
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying){
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                // Gradiente
+                VideoBackground(stops: [0.8,1.0],),
+                // Texto
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption)
+                  )
+              ],
+            ),
           ),
         );
       }, 
