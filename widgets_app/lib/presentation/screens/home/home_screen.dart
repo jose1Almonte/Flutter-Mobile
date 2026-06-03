a1c5f49aadc044125a158ff44a2550c40775ce09
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-06-03 12:36:21 -0400

    Update home_screen.dart

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
 
