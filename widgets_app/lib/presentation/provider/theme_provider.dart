1a72f4093890d76f494aa9fb0530e5e5aaf02b11
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-06-22 15:48:38 -0400

    StateNotifierProvider

--- widgets_app/lib/presentation/provider/theme_provider.dart
+++ widgets_app/lib/presentation/provider/theme_provider.dart
@@ -3,11 +3,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-// Un simple boolean
-final isDarkModeProvider = StateProvider<bool>((ref) => false,);
-
 // Listado de colores inmutable
 final colorListProvider = Provider((ref) => colorList,);
 
+// Un simple boolean
+final isDarkModeProvider = StateProvider<bool>((ref) => false,);
+
 // Un simple int
 final selectedColorProvider = StateProvider<int>((ref) => 0,);
+
+// un objeto de tipo AppTheme (custom)
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+  );
+
+// Controller o Notifier
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  // STATE = Estado = new AppTheme();
+  
+  ThemeNotifier(): super(AppTheme());
+  // ThemeNotifier(super.state);
+
+}
