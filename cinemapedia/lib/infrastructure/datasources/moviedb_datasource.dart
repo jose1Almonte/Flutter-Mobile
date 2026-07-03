72f2f69a7c144843b6137d960f2a94bde1c930a2
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-07-03 19:11:27 -0400

    MovieDB datasource, mapper y repositorio implementados

--- cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,5 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 import 'package:cinemapedia/config/constants/environment.dart';
@@ -5,23 +7,26 @@ import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
-  final dio = Dio(BaseOptions(
-    baseUrl: "https://api.themoviedb.org/3",
-    queryParameters: {
-      'api_key': Environment.theMovieDbKey,
-      'language': 'es-MX'
-    }));
-  
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: "https://api.themoviedb.org/3",
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX',
+      },
+    ),
+  );
+
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
 
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
 
-
-
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster',)
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
-
 }
