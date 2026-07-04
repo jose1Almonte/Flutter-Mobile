ae1fa3b13a210398a3572ab44f28032ed46a24e0
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-07-04 11:32:15 -0400

    autoplay activado en MoviesSlideshow

--- cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
+++ cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
@@ -15,6 +15,7 @@ class MoviesSlideshow extends StatelessWidget {
       child: Swiper(
         viewportFraction: 0.8,
         scale: 0.9,
+        autoplay: true,
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
